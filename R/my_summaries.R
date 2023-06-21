e3b50f1f045e64c25c22c2642f0c8d070cbbb240
-e <daren-kuwaye@uiowa.edu>
2023-06-20 23:00:08 -0500

    Function documentation added with roxygen2.

--- R/my_summaries.R
+++ R/my_summaries.R
@@ -1,4 +1,15 @@
-# This function creates a summary for a numeric vector
+#' Summaries for numeric vectors
+#'
+#' @param x vector.
+#' @param na.rm passed into other functions.
+#'
+#' @return a numeric vector with min, max, mean, sd, length and number of
+#'  missing values.
+#' @export
+#'
+#' @examples
+#' nvec <- c(2022, 2021, NA, 2021, 2021, NA, 2022)
+#' numeric_summary(nvec, na.rm = TRUE)
 numeric_summary <- function(x, na.rm = FALSE){
 
   min <- min(x, na.rm = na.rm)
@@ -11,8 +22,18 @@ numeric_summary <- function(x, na.rm = FALSE){
   c(min = min, max = max, mean = mean, sd = sd, length = length, Nmiss = Nmiss)
 }
 
-# This function creates a summary for a character vector
-char_summary <- function(x, na.rm = FALSE){
+#' Summaries for character vectors
+#'
+#' @param x vector.
+#'
+#' @return a numeric vector of length, number of missing values and number of
+#'  unique values.
+#' @export
+#'
+#' @examples
+#' cvec <- c("Boston", NA, "Brookline", "Brighton", NA, "Boston")
+#' char_summary(cvec)
+char_summary <- function(x){
 
   length <- length(x)
   Nmiss <- sum(is.na(x))
